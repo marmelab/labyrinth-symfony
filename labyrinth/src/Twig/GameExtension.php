45e103d685b3cc8d3f17b566e3d774ef9406bf90
pem <pierre-etienne.moreau@loria.fr>
2018-10-03 16:15:15 +0200

    draw a board using texture

---
+++ labyrinth/src/Twig/GameExtension.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class GameExtension extends AbstractExtension
+{
+    public function getFunctions()
+    {
+        return array(
+            new TwigFunction('tileTypeToMatrix', array($this, 'tileTypeToMatrix')),
+        );
+    }
+
+    public function tileTypeToMatrix($tileType)
+    {
+        if ($tileType == "┗") {
+            return [[1,0,1],[1,0,0],[1,1,1]];
+        } elseif ($tileType == "┻") {
+            return [[1,0,1],[0,0,0],[1,1,1]];
+        } elseif ($tileType == "┃") {
+            return [[1,0,1],[1,0,1],[1,0,1]];
+        }
+    }
+}
