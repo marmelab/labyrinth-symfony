90901dd51af847ab3c9c355cfd9c005ae52ff4ca
pem <pierre-etienne.moreau@loria.fr>
2018-10-08 16:43:56 +0200

    renamed route

--- labyrinth/src/Controller/GameController.php
+++ labyrinth/src/Controller/GameController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
@@ -25,11 +26,22 @@ class GameController extends AbstractController
     }
 
     /**
-     * @Route("/rotate")
+     * @Route("/rotateRemainingPathCard/{x<\d+>}/{y<\d+>}", name="rotateRemainingPathCard")
      */
-    public function rotate()
+    public function rotateRemainingPathCard($x, $y)
     {
-        $game = $this->gameApi->rotate($this->gameApi->createGame());
+        // need to retrieve a game here: $this->gameRepository->findGameById($idGame);
+        $game = $this->gameApi->rotateRemainingPathCard($this->gameApi->createGame(), $x, $y);
+        return $this->render('game.html.twig', ['game' => $game]);
+    }
+
+    /**
+     * @Route("/insertRemainingPathCard/{x<\d+>}/{y<\d+>}", name="insertRemainingPathCard")
+     */
+    public function insertRemainingPathCard($x, $y)
+    {
+        // need to retrieve a game here: $this->gameRepository->findGameById($idGame);
+        $game = $this->gameApi->rotateRemainingPathCard($this->gameApi->createGame(), $x, $y);
         return $this->render('game.html.twig', ['game' => $game]);
     }
 }
