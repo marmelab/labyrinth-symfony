a62bfe19054d892cc914a183d0cbbc11cba44c34
pem <pierre-etienne.moreau@loria.fr>
2018-10-08 15:30:36 +0200

    added a test for fromBoardReferentialToDisplay

--- labyrinth/tests/Twig/GameExtensionTest.php
+++ labyrinth/tests/Twig/GameExtensionTest.php
@@ -29,4 +29,32 @@ class GameExtensionTest extends TestCase
             $this->assertEquals(3, count($row));
         }
     }
+
+    public function testFromBoardReferentialToDisplay()
+    {
+        $gameExtension = new GameExtension();
+        // board referential: 'y' is going upward from -1 to 7
+        // display referential : 'y' is going downward from 1 to 9
+        $lowerLeftCorner = $gameExtension->fromBoardReferentialToDisplay([0,0]);
+        $upperLeftCorner = $gameExtension->fromBoardReferentialToDisplay([0,6]);
+        $lowerRightCorner = $gameExtension->fromBoardReferentialToDisplay([6,0]);
+        $upperRightCorner = $gameExtension->fromBoardReferentialToDisplay([6,6]);
+        $remainingPathCardPosition = $gameExtension->fromBoardReferentialToDisplay([1,-1]);
+
+        $this->assertEquals(2, $lowerLeftCorner[0]);
+        $this->assertEquals(8, $lowerLeftCorner[1]);
+
+        $this->assertEquals(2, $upperLeftCorner[0]);
+        $this->assertEquals(2, $upperLeftCorner[1]);
+
+        $this->assertEquals(8, $lowerRightCorner[0]);
+        $this->assertEquals(8, $lowerRightCorner[1]);
+
+        $this->assertEquals(8, $upperRightCorner[0]);
+        $this->assertEquals(2, $upperRightCorner[1]);
+
+        $this->assertEquals(3, $remainingPathCardPosition[0]);
+        $this->assertEquals(9, $remainingPathCardPosition[1]);
+    }
+
 }
